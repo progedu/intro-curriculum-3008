6b234c311a39a238c0be4f0992f4915ea79b2889
Tsubasa-Sato <tsubasa_16n1200394@nnn.ed.jp>
2016-09-23 12:56:10 +0000

    一覧が空のときに発言

--- scripts/todo.js
+++ scripts/todo.js
@@ -25,9 +25,19 @@ module.exports = (robot) => {
 		msg.send('削除しました: ' + task);
 	});
 	robot.respond(/list/i, (msg) => {
-		msg.send(todo.list().join('\n'));
+		const taskList = todo.list();
+		if (taskList.length === 0) {
+			msg.send('(TODOはありません)');
+		} else{
+			msg.send(todo.list().join('\n'));
+		}
 	});
 	robot.respond(/donelist/i, (msg) => {
-		msg.send(todo.donelist().join('\n'));
+		const doneList = todo.donelist();
+		if (todo.length === 0) {
+			msg.send('(完了したTODOはありません)')
+		} else{
+			msg.send(todo.donelist().join('\n'));
+		}
 	});
 };
