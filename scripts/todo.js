321833e89d8a33dbeff66e1f3e28b28155b18992
Yukimasa Shimada <github@eleos.jp>
2016-09-23 12:59:14 +0000

    タスクが無かったらありませんとコメントする機能を追加

--- scripts/todo.js
+++ scripts/todo.js
@@ -25,9 +25,20 @@ module.exports = (robot) => {
 		msg.send('削除しました: ' + task);
 	});
 	robot.respond(/list/i, (msg) => {
-		msg.send(todo.list().join('\n'));
+		const tasklist = todo.list();
+		if (tasklist.length === 0) {
+			msg.send('(TODOはありません)');
+		} else {
+			msg.send(todo.list().join('\n'));
+		}
 	});
+
 	robot.respond(/donelist/i, (msg) => {
-		msg.send(todo.donelist().join('\n'));
+		const tasklist = todo.list();
+		if (tasklist.length === 0) {
+			msg.send('(完了したTODOはありません)');
+		} else {
+			msg.send(todo.donelist().join('\n'));
+		}
 	});
 };
