f1dd27f541ec9810e68e25d699b0809a786a1c30
Akimoto Daichi <daichi_akimoto@dwango.co.jp>
2016-12-27 09:01:57 +0000

    list()とdonelist()の混同を修正

--- scripts/todo.js
+++ scripts/todo.js
@@ -30,15 +30,15 @@ module.exports = (robot) => {
 		if (list.length === 0) {
 			msg.send('TODOはありません。');
 		} else {
-			msg.send(todo.list().join('\n'));
+			msg.send(list().join('\n'));
 		}
 	});
 	robot.respond(/donelist/i, (msg) => {
-		const list = todo.list();
-		if (list.length === 0) {
+		const donelist = todo.donelist();
+		if (donelist.length === 0) {
 			msg.send('完了したTODOはありません。');
 		} else {
-			msg.send(todo.list().join('\n'));
+			msg.send(donelist().join('\n'));
 		}
 	});
 };
