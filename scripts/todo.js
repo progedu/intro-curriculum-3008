bf7ca6397b6fd1d486c17ab75c74d3d0faa6072f
norielu <nrc41826.nifty.com>
2017-01-23 05:25:03 +0000

    list,donelist項目内の記述を訂正(宣言変数に合わせる)

--- scripts/todo.js
+++ scripts/todo.js
@@ -29,7 +29,7 @@ module.exports = (robot) => {
 		if(tasklist.length === 0) {
 			msg.send('(TODOはありません)');
 		}else{			
-			msg.send(todo.list().join('\n'));
+			msg.send(tasklist.join('\n'));
 		}
 	});
 	robot.respond(/donelist/i, (msg) => {
@@ -37,7 +37,7 @@ module.exports = (robot) => {
 		if(taskdonelist.length === 0) {
 			msg.send('(完了したTODOはありません)');			
 		}else{
-			msg.send(todo.donelist().join('\n'));			
+			msg.send(taskdonelist.join('\n'));			
 		}
 	});
 };
