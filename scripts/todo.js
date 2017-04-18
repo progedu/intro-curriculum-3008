f35a412ca2ad3e7c556f9435322ce2ba61b87ef9
lakusui <vau.qi.55@gmail.com>
2017-04-18 12:27:18 +0000

    ボット発言を追加

--- scripts/todo.js
+++ scripts/todo.js
@@ -25,9 +25,19 @@ module.exports = (robot) => {
 		msg.send('削除しました: ' + task);
 	});
 	robot.respond(/list/i, (msg) => {
-		msg.send(todo.list().join('(TODOはありません)'));
+		const list = todo.list();
+		if (list.length === 0) {
+			msg.send('(TODOはありません)')；
+		}else {
+			msg.send(list.join('\n'));
+		}
 	});
 	robot.respond(/donelist/i, (msg) => {
-		msg.send(todo.donelist().join('(完了したTODOはありません)'));
+		const donelist = todo.donelist();
+		if (donelist.length === 0) {
+			msg.send('(完了したTODOはありません)');
+		}else {
+			msg.send(donelist.join('\n'));
+		}
 	});
 };
