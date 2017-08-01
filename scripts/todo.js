0743184309d18005024b14d47e07d0df7930f9d5
maicine <healthya92@gmail.com>
2017-08-01 02:21:25 +0000

    タスクがない場合の処理を追加

--- scripts/todo.js
+++ scripts/todo.js
@@ -1,11 +1,11 @@
 // Description:
-//   TODO を管理することができるボットです
+//	TODO を管理することができるボットです
 // Commands:
-//   ボット名 todo     - TODO を作成
-//   ボット名 done     - TODO を完了にする
-//   ボット名 del      - TODO を消す
-//   ボット名 list     - TODO の一覧表示
-//   ボット名 donelist - 完了した TODO の一覧表示
+//	ボット名 todo     - TODO を作成
+//	ボット名 done     - TODO を完了にする
+//	ボット名 del      - TODO を消す
+//	ボット名 list     - TODO の一覧表示
+//	ボット名 donelist - 完了した TODO の一覧表示
 'use strict';
 const todo = require('todo');
 module.exports = (robot) => {
@@ -25,9 +25,22 @@ module.exports = (robot) => {
 		msg.send('削除しました: ' + task);
 	});
 	robot.respond(/list/i, (msg) => {
-		msg.send(todo.list().join('\n'));
+		const list = todo.list();
+		if(list.length === 0){
+			msg.send('(TODOはありません)');
+		}else{
+			msg.send(list.join('\n'));
+		}
 	});
 	robot.respond(/donelist/i, (msg) => {
-		msg.send(todo.donelist().join('\n'));
+		const donelist = todo.donelist();
+		if(donelist.length === 0){
+			msg.send('(完了したTODOはありません)');
+		}else{
+			msg.send(donelist.join('\n'));
+		}
 	});
+
 };
+
+//ボット名はintro-curriculum-3008ではなく元データでyo hubotしたときに名付けたhubot-todoである。
