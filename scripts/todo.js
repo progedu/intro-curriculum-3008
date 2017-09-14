9640767bba194ea369ca540485989ca54475b838
207-208 <208_209@outlook.jp>
2017-09-14 11:47:09 +0000

    listが空の時にメッセージを返す

--- scripts/todo.js
+++ scripts/todo.js
@@ -6,28 +6,43 @@
 //   ボット名 del      - TODO を消す
 //   ボット名 list     - TODO の一覧表示
 //   ボット名 donelist - 完了した TODO の一覧表示
+//   ボット名 clear    - TODO をすべて消す
 'use strict';
 const todo = require('todo');
 module.exports = (robot) => {
-	robot.respond(/todo (.+)/i, (msg) => {
+	robot.hear(/todo (.+)/i, (msg) => {
 		const task = msg.match[1].trim();
 		todo.todo(task);
 		msg.send('追加しました: ' + task);
 	});
-	robot.respond(/done (.+)/i, (msg) => {
+	robot.hear(/done (.+)/i, (msg) => {
 		const task = msg.match[1].trim();
 		todo.done(task);
 		msg.send('完了にしました: ' + task);
 	});
-	robot.respond(/del (.+)/i, (msg) => {
+	robot.hear(/del (.+)/i, (msg) => {
 		const task = msg.match[1].trim();
 		todo.del(task);
 		msg.send('削除しました: ' + task);
 	});
-	robot.respond(/list/i, (msg) => {
-		msg.send(todo.list().join('\n'));
+	robot.hear(/list/i, (msg) => {
+		const list = todo.list();
+		if (list.length === 0) {
+			msg.send('(TODOはありません)');
+		} else {
+			msg.send(list.join('\n'));
+		}
 	});
-	robot.respond(/donelist/i, (msg) => {
-		msg.send(todo.donelist().join('\n'));
+	robot.hear(/donelist/i, (msg) => {
+		const doneList = todo.donelist();
+		if (doneList.length === 0) {
+			msg.send('(完了したTODOはありません)');
+		} else {
+			msg.send(doneList.join('\n'));
+		}
 	});
+	robot.hear(/clear/i, (msg) => {
+    todo.clearAll(); // tasks.clear();
+    msg.send('(TODOを初期化しました)');
+  });
 };
