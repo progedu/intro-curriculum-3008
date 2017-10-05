0d9914066e6fd42fab2db96c7dca0baaec9c8736
taichi <taichilovesspitz@yahoo.co.jp>
2017-10-05 14:52:00 +0000

    TODOリストとDONEリストが空のときの返信を追加

--- scripts/todo.js
+++ scripts/todo.js
@@ -8,6 +8,7 @@
 //   ボット名 donelist - 完了した TODO の一覧表示
 'use strict';
 const todo = require('todo');
+const assert = require('assert');
 module.exports = (robot) => {
 	robot.respond(/todo (.+)/i, (msg) => {
 		const task = msg.match[1].trim();
@@ -25,9 +26,17 @@ module.exports = (robot) => {
 		msg.send('削除しました: ' + task);
 	});
 	robot.respond(/list/i, (msg) => {
-		msg.send(todo.list().join('\n'));
+		if (assert.deepEqual(todo.list(), [])) {
+			msg.send(todo.list().join('\n'));	
+		} else {
+			msg.send('(TODOはありません)');
+		};
 	});
 	robot.respond(/donelist/i, (msg) => {
-		msg.send(todo.donelist().join('\n'));
+		if (assert.deepEqual(todo.donelist(), [])) {
+			msg.send(todo.donelist().join('\n'));
+		} else {
+			msg.send('(完了したTODOはありません)');
+		}
 	});
 };
