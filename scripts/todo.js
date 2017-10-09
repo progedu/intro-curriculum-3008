b42e748710cf7eab33481033cb403e766258be78
gadget-tf <gadget.tf@gmail.com>
2017-10-09 03:09:24 +0000

    練習問題、空配列の場合の対応の実装

--- scripts/todo.js
+++ scripts/todo.js
@@ -25,9 +25,14 @@ module.exports = (robot) => {
 		msg.send('削除しました: ' + task);
 	});
 	robot.respond(/list/i, (msg) => {
-		msg.send(todo.list().join('\n'));
+		if (todo.list().length === 0) {
+			msg.send('(TODOはありません)');
+		} else {
+		    msg.send(todo.list().join('\n'));
+		}
 	});
 	robot.respond(/donelist/i, (msg) => {
-		msg.send(todo.donelist().join('\n'));
+		let message = todo.donelist().join('\n') || '(完了したTODOはありません)';
+		msg.send(message);
 	});
 };
