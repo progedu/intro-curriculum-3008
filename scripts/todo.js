58c86e3aafc9b4be61f9b275a47f250d7019b170
hukuti145 <hukuti145@gmail.com>
2017-11-04 13:27:06 +0000

    TODOがない場合の発言を追加

--- scripts/todo.js
+++ scripts/todo.js
@@ -25,9 +25,32 @@ module.exports = (robot) => {
 		msg.send('削除しました: ' + task);
 	});
 	robot.respond(/list/i, (msg) => {
+		/*
 		msg.send(todo.list().join('\n'));
+		if (todo.list().length === 0) {
+			msg.send('(TODOはありません)');
+		}
+		*/
+		const list = todo.list();
+		if (list.length === 0) {
+			msg.send('(TODOはありません)');
+		} else {
+			msg.send(list.join('\n'));
+		}
+
 	});
 	robot.respond(/donelist/i, (msg) => {
+		/*
 		msg.send(todo.donelist().join('\n'));
+		if (todo.donelist().length === 0) {
+			msg.send('(完了したTODOはありません)');
+		}
+		*/
+        const donelist = todo.donelist();
+        if (donelist.length === 0) {
+            msg.send('(完了したTODOはありません)');
+        } else {
+            msg.send(donelist.join('\n'));
+        }
 	});
 };
