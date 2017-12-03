b4210a256c9e505ea4acb8c5dd549f91bda4e86f
sunao ohta <mr_99_sunao@yahoo.co.jp>
2017-12-03 06:26:37 +0000

    リストが存在しない場合の処理追加

--- scripts/todo.js
+++ scripts/todo.js
@@ -25,9 +25,19 @@ module.exports = (robot) => {
 		msg.send('削除しました: ' + task);
 	});
 	robot.respond(/list/i, (msg) => {
-		msg.send(todo.list().join('\n'));
+		let output = todo.list().join('\n');
+		if(output) {
+			msg.send(output);
+		} else {
+			msg.send('TODOはありません。')
+		}
 	});
 	robot.respond(/donelist/i, (msg) => {
-		msg.send(todo.donelist().join('\n'));
+		let output = todo.doneList().join('\n');
+		if(output) {
+			msg.send(output);
+		} else {
+			msg.send('完了したTODOはありません。')
+		}
 	});
 };
