d5a5662e8f21b8d8ae6bddf365160d015dace536
hagiwara <keep.my.best.61@gmail.com>
2017-12-29 08:59:53 +0000

    リストが空の場合もメッセージを返すようにした

--- scripts/todo.js
+++ scripts/todo.js
@@ -25,9 +25,19 @@ module.exports = (robot) => {
 		msg.send('削除しました: ' + task);
 	});
 	robot.respond(/list/i, (msg) => {
-		msg.send(todo.list().join('\n'));
+		const arr = todo.list();
+		if(arr.length === 0) {
+			msg.send('(TODOはありません)');
+		} else {
+			msg.send(arr.join('\n'));
+		}
 	});
 	robot.respond(/donelist/i, (msg) => {
-		msg.send(todo.donelist().join('\n'));
+		const arr = todo.donelist();
+		if(arr.length === 0) {
+			msg.send('(完了したTODOはありません)');
+		} else {
+			msg.send(arr.join('\n'));
+		}
 	});
 };
