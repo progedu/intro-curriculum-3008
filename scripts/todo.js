be9ec3381c9287ba15159bb33503af2f5b942c93
umaada <la.lune.nasu@gmail.com>
2018-06-21 09:26:11 +0000

    リストの中身がない場合の表示

--- scripts/todo.js
+++ scripts/todo.js
@@ -25,9 +25,15 @@ module.exports = (robot) => {
 		msg.send('削除しました: ' + task);
 	});
 	robot.respond(/list/i, (msg) => {
-		msg.send(todo.list().join('\n'));
+		if(!msg.send(todo.list().join('\n')))
+		{
+			msg.send('TODOはありません');
+		}
 	});
 	robot.respond(/donelist/i, (msg) => {
-		msg.send(todo.donelist().join('\n'));
+		if(!msg.send(todo.donelist().join('\n')))
+		{
+			msg.send('完了したTODOはありません');
+		}
 	});
 };
