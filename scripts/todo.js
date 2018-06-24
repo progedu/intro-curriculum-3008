06181667a140c96a1144c822cf02e5d6aa2cb5c3
seri1234 <abc_x_y_z_12@yahoo.co.jp>
2018-06-24 08:16:42 +0000

    ボットインターフェイスのと連携練習課題todo donelist表示実装

--- scripts/todo.js
+++ scripts/todo.js
@@ -9,11 +9,11 @@
 'use strict';
 const todo = require('todo');
 module.exports = (robot) => {
-	robot.respond(/todo (.+)/i, (msg) => {
-		const task = msg.match[1].trim();
-		todo.todo(task);
-		msg.send('追加しました: ' + task);
-	});
+    robot.respond(/todo (.+)/i, (msg) => {
+        const task = msg.match[1].trim();
+        todo.todo(task);
+        msg.send('追加しました: ' + task);
+    });
 	robot.respond(/done (.+)/i, (msg) => {
 		const task = msg.match[1].trim();
 		todo.done(task);
@@ -25,9 +25,19 @@ module.exports = (robot) => {
 		msg.send('削除しました: ' + task);
 	});
 	robot.respond(/list/i, (msg) => {
+		if(todo.list().length===0){
+			msg.send('(TODOはありません)');
+		}else{
 		msg.send(todo.list().join('\n'));
+		}
 	});
+
 	robot.respond(/donelist/i, (msg) => {
+		if(todo.donelist().length === 0){
+			msg.send('(完了したTODOはありません)');
+		}else{
 		msg.send(todo.donelist().join('\n'));
+	
+		}
 	});
 };
