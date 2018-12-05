085a546910943bf9d38e1164141241cfb612863c
Cessna-Key-Tree <shin1ho4@gmail.com>
2018-12-05 09:04:26 +0000

    list, donelist が空の時コメント返信

--- scripts/todo.js
+++ scripts/todo.js
@@ -1,11 +1,11 @@
 // Description:
-//   TODO を管理することができるボットです
+//   ToDo を管理することができるボットです
 // Commands:
-//   ボット名 todo     - TODO を作成
-//   ボット名 done     - TODO を完了にする
-//   ボット名 del      - TODO を消す
-//   ボット名 list     - TODO の一覧表示
-//   ボット名 donelist - 完了した TODO の一覧表示
+//   ボット名 todo     - ToDo を作成
+//   ボット名 done     - ToDo を完了にする
+//   ボット名 del      - ToDo を消す
+//   ボット名 list     - ToDo の一覧表示
+//   ボット名 donelist - 完了した ToDo の一覧表示
 'use strict';
 const todo = require('todo');
 module.exports = (robot) => {
@@ -25,9 +25,19 @@ module.exports = (robot) => {
 		msg.send('削除しました: ' + task);
 	});
 	robot.respond(/list/i, (msg) => {
-		msg.send(todo.list().join('\n'));
+		let list = todo.list();
+		if (list.length === 0) {
+			msg.send('ToDo はありません');
+		} else {
+			msg.send(todo.list().join('\n'));
+		}
 	});
 	robot.respond(/donelist/i, (msg) => {
+		let donelist = todo.donelist();
+		if (donelist.length === 0) {
+			msg.send('完了した ToDo はありません');
+		} else {
 		msg.send(todo.donelist().join('\n'));
+		}
 	});
 };
