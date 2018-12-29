33c67420075c4dd5e3f903c33c04ca4e13f2a4bb
tiot07 <tiot7155@gmail.com>
2018-12-30 01:26:58 +0900

    修正

--- scripts/todo.js
+++ scripts/todo.js
@@ -25,20 +25,19 @@ module.exports = (robot) => {
 		msg.send('削除しました: ' + task);
 	});
 	robot.respond(/list/i, (msg) => {
-		if(list.length === 0){
-			msg.send('(todoはありません)');
-		}else{
-			msg.send(todo.list().join('\n'));
-		}
-	});
-	robot.respond(/donelist/i, (msg) => {
-		if(donelist.length === 0){
-			msg.send('(完了したtodoはありません)');
-		}else{
-		msg.send(todo.donelist().join('\n'));
-		}
-	});
+        const list = todo.list();
+        if (list.length === 0) {
+            msg.send('(TODOはありません)');
+        } else {
+            msg.send(list.join('\n'));
+        }
+    });
+    robot.respond(/donelist/i, (msg) => {
+        const donelist = todo.donelist();
+        if (donelist.length === 0) {
+            msg.send('(完了したTODOはありません)');
+        } else {
+            msg.send(donelist.join('\n'));
+        }
+    });
 };
-
-
-//test
