657c66a1a5967c94c2deafaf326955dac8836539
s-nishikoori <s-nishikoori@xware.co.jp>
2019-04-16 13:04:23 +0900

    練習

--- scripts/todo.js
+++ scripts/todo.js
@@ -25,9 +25,17 @@ module.exports = (robot) => {
 		msg.send('削除しました: ' + task);
 	});
 	robot.respond(/list/i, (msg) => {
-		msg.send(todo.list().join('\n'));
+		if(todo.list()[0] == null){
+			msg.send('(TODOはありません)');
+		}else{
+			msg.send(todo.list().join('\n'));
+		}
 	});
 	robot.respond(/donelist/i, (msg) => {
-		msg.send(todo.donelist().join('\n'));
+		if(todo.donelist()[0] == null){
+			msg.send('(完了したTODOはありません)');
+		}else{
+			msg.send(todo.donelist().join('\n'));
+		}
 	});
 };
