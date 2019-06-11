342ff6f0ad4a1ef431a615e8fb6dac9639737d73
AyasaFushimi <bolbus0617@gmail.com>
2019-06-11 22:41:19 +0900

    list/donelistが空だった場合の処理

--- scripts/todo.js
+++ scripts/todo.js
@@ -5,29 +5,35 @@
 //   ボット名 done     - TODO を完了にする
 //   ボット名 del      - TODO を消す
 //   ボット名 list     - TODO の一覧表示
-//   ボット名 donelist - 完了した TODO の一覧表示
+//   ボット名 donelist - TODO 完了した TODO の一覧表示
 'use strict';
 const todo = require('todo');
 module.exports = (robot) => {
-	robot.respond(/todo (.+)/i, (msg) => {
-		const task = msg.match[1].trim();
-		todo.todo(task);
-		msg.send('追加しました: ' + task);
-	});
-	robot.respond(/done (.+)/i, (msg) => {
-		const task = msg.match[1].trim();
-		todo.done(task);
-		msg.send('完了にしました: ' + task);
-	});
-	robot.respond(/del (.+)/i, (msg) => {
-		const task = msg.match[1].trim();
-		todo.del(task);
-		msg.send('削除しました: ' + task);
-	});
-	robot.respond(/list/i, (msg) => {
-		msg.send(todo.list().join('\n'));
-	});
-	robot.respond(/donelist/i, (msg) => {
-		msg.send(todo.donelist().join('\n'));
+    robot.respond(/todo (.+)/i, (msg) => {
+        const task = msg.match[1].trim();
+        todo.todo(task);
+        msg.send('追加しました: ' + task);
+    });
+    robot.respond(/done (.+)/i, (msg) => {
+        const task = msg.match[1].trim();
+        todo.done(task);
+        msg.send('完了にしました: ' + task);
+    });
+    robot.respond(/del (.+)/i, (msg) => {
+        const task = msg.match[1].trim();
+        todo.del(task);
+        msg.send('削除しました: ' + task);
+    });
+    robot.respond(/list/i, (msg) => {
+        msg.send(todo.list().join('\n'));
+        if(todo.list.length === 0){
+            msg.send('(TODOはありません)');
+        }
+    });
+    robot.respond(/donelist/i, (msg) => {
+        msg.send(todo.donelist().join('\n'));
+        if(todo.donelist.length === 0){
+            msg.send('(完了したTODOはありません)');
+        }
 	});
 };
