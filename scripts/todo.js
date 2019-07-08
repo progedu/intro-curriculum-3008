ce855a4ef73f65a7dc7969cba54fb03f010cd598
touei10 <52106355+touei10@users.noreply.github.com>
2019-07-08 20:42:07 +0900

    修正：リストが空の場合

--- scripts/todo.js
+++ scripts/todo.js
@@ -3,31 +3,47 @@
 // Commands:
 //   ボット名 todo     - TODO を作成
 //   ボット名 done     - TODO を完了にする
+//   ボット名 undone     - 完了らか TODO リストに戻す
 //   ボット名 del      - TODO を消す
 //   ボット名 list     - TODO の一覧表示
 //   ボット名 donelist - 完了した TODO の一覧表示
 'use strict';
 const todo = require('todo');
 module.exports = (robot) => {
-	robot.respond(/todo (.+)/i, (msg) => {
-		const task = msg.match[1].trim();
-		todo.todo(task);
-		msg.send('追加しました: ' + task);
-	});
-	robot.respond(/done (.+)/i, (msg) => {
-		const task = msg.match[1].trim();
-		todo.done(task);
-		msg.send('完了にしました: ' + task);
-	});
-	robot.respond(/del (.+)/i, (msg) => {
-		const task = msg.match[1].trim();
-		todo.del(task);
-		msg.send('削除しました: ' + task);
-	});
-	robot.respond(/list/i, (msg) => {
-		msg.send(todo.list().join('\n'));
-	});
-	robot.respond(/donelist/i, (msg) => {
-		msg.send(todo.donelist().join('\n'));
-	});
+    robot.respond(/todo (.+)/i, (msg) => {
+        const task = msg.match[1].trim();
+        todo.todo(task);
+        msg.send('追加しました: ' + task);
+    });
+    robot.respond(/done (.+)/i, (msg) => {
+        const task = msg.match[1].trim();
+        todo.done(task);
+        msg.send('完了にしました: ' + task);
+    });
+    robot.respond(/undone (.+)/i, (msg) => {
+        const task = msg.match[1].trim();
+        todo.undone(task);
+        msg.send('TODO リストに戻しました: ' + task);
+    });
+    robot.respond(/del (.+)/i, (msg) => {
+        const task = msg.match[1].trim();
+        todo.del(task);
+        msg.send('削除しました: ' + task);
+    });
+    robot.respond(/list/i, (msg) => {
+        const list = todo.list();
+        if (list.length === 0) {
+            msg.send('(TODO はありません)');
+        } else {
+            msg.send(list.join('\n'));
+        }
+    });
+    robot.respond(/donelist/i, (msg) => {
+        const donelist = todo.donelist();
+        if (donelist.length === 0) {
+            msg.send('(完了した TODO はありません)');
+        } else {
+            msg.send(donelist.join('\n'));
+        }
+    });
 };
