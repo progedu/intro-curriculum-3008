e564cff9d5ca265168a269dbaf97fae6c9d01436
atsushi3r <atsushi.sato.3r@gmail.com>
2019-08-26 16:02:53 +0900

    インデントをタブに変更

--- scripts/todo.js
+++ scripts/todo.js
@@ -25,17 +25,17 @@ module.exports = (robot) => {
 		msg.send('削除しました: ' + task);
 	});
 	robot.respond(/list/i, (msg) => {
-        if (todo.list().length) {
-            msg.send(todo.list().join('\n'));
-        } else {
-            msg.send('(TODOはありません)');
-        }
+		if (todo.list().length) {
+			msg.send(todo.list().join('\n'));
+		} else {
+			msg.send('(TODOはありません)');
+		}
 	});
 	robot.respond(/donelist/i, (msg) => {
-        if (todo.donelist().length) {
-            msg.send(todo.donelist().join('\n'));
-        } else {
-            msg.send('(完了したTODOはありません)');
-        }
+		if (todo.donelist().length) {
+			msg.send(todo.donelist().join('\n'));
+		} else {
+			msg.send('(完了したTODOはありません)');
+		}
 	});
 };
