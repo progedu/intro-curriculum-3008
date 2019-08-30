57b777e2aa84d1016318dc9fb7101f639ebdffeb
Haruna Karasawa <haruna_karasawa@nnn.ac.jp>
2019-08-30 10:04:44 +0900

    完了と未完了リストが空の時のメッセージを追加

--- scripts/todo.js
+++ scripts/todo.js
@@ -25,9 +25,17 @@ module.exports = (robot) => {
 		msg.send('削除しました: ' + task);
 	});
 	robot.respond(/list/i, (msg) => {
-		msg.send(todo.list().join('\n'));
+		if (todo.list() == '') {
+			msg.send('(TODOはありません)\n');
+		}else{
+			msg.send(todo.list().join('\n'));
+		}
 	});
 	robot.respond(/donelist/i, (msg) => {
-		msg.send(todo.donelist().join('\n'));
+		if (todo.donelist() == '') {
+			msg.send('(完了したTODOはありません)\n');
+		}else{
+			msg.send(todo.donelist().join('\n'));
+		}
 	});
 };
