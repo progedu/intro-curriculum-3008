8aa87d5b51baf5539c68ac52fa7ae49c4b69898e
kazuya-hub <56819991+kazuya-hub@users.noreply.github.com>
2019-10-24 15:38:02 +0900

    listコマンドとdonelistコマンドの機能追加
    listコマンドとdonelistコマンドにおいて、表示する項目が一つも無い場合はその旨を発言するようにしました

--- scripts/todo.js
+++ scripts/todo.js
@@ -25,9 +25,19 @@ module.exports = (robot) => {
 		msg.send('削除しました: ' + task);
 	});
 	robot.respond(/list/i, (msg) => {
-		msg.send(todo.list().join('\n'));
+    const list = todo.list();
+    if (list.length === 0) {
+      msg.send('TODOはありません');
+    } else {
+		  msg.send(list.join('\n'));
+    }
 	});
 	robot.respond(/donelist/i, (msg) => {
-		msg.send(todo.donelist().join('\n'));
+    const done_list = todo.donelist();
+    if (done_list.length === 0) {
+      msg.send('完了したTODOはありません');
+    } else {
+		  msg.send(done_list.join('\n'));
+    }
 	});
 };
