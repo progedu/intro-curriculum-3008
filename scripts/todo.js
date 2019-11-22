cca07a45f49b4eae3182780f3e859235900dc0c5
ichijikuboy <kubo_june@yahoo.co.jp>
2019-11-22 09:33:09 +0000

    listが空のときの処理

--- scripts/todo.js
+++ scripts/todo.js
@@ -25,9 +25,17 @@ module.exports = (robot) => {
 		msg.send('削除しました: ' + task);
 	});
 	robot.respond(/list/i, (msg) => {
-		msg.send(todo.list().join('\n'));
+		if (msg.send()) {
+			msg.send(todo.list().join('\n'));
+		} else {
+			msg.send('（TODOはありません）');
+		}
 	});
 	robot.respond(/donelist/i, (msg) => {
-		msg.send(todo.donelist().join('\n'));
+		if (msg.send()) {
+			msg.send(todo.donelist().join('\n'));
+		} else {
+			msg.send('（完了したTODOはありません）');
+		}
 	});
 };
