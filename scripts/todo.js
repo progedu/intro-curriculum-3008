023bb7dbae4b523af696256bfe5bc9fa5b1a0228
Hisashi FURUTA <mail@furutahisashi.com>
2020-01-11 21:16:16 +0900

    TODOが存在しない場合のアナウンス追加

--- scripts/todo.js
+++ scripts/todo.js
@@ -25,9 +25,19 @@ module.exports = (robot) => {
 		msg.send('削除しました: ' + task);
 	});
 	robot.respond(/list/i, (msg) => {
-		msg.send(todo.list().join('\n'));
+		const tasks = todo.list();
+		if(tasks.length) {
+			msg.send(tasks.join('\n'));
+		} else {
+			msg.send('(TODOはありません)');
+		}
 	});
 	robot.respond(/donelist/i, (msg) => {
-		msg.send(todo.donelist().join('\n'));
+		const tasks = todo.donelist();
+		if(tasks.length) {
+			msg.send(todo.donelist().join('\n'));
+		} else {
+			msg.send('(完了したTODOはありません)');
+		}
 	});
 };
