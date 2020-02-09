6b33c221f3b0a28a8e8d5ad684ae17258b0d9364
lvflwr <mayhem123757@gmail.com>
2020-02-09 18:38:56 +0000

    リストが空であるときの通知機能を追加

--- scripts/todo.js
+++ scripts/todo.js
@@ -25,9 +25,19 @@ module.exports = (robot) => {
 		msg.send('削除しました: ' + task);
 	});
 	robot.respond(/list/i, (msg) => {
-		msg.send(todo.list().join('\n'));
+		const list = todo.list();
+		if (list.length !== 0) {
+			msg.send(list.join('\n'));
+		} else {
+			msg.send('(TODOはありません)');
+		}
 	});
 	robot.respond(/donelist/i, (msg) => {
-		msg.send(todo.donelist().join('\n'));
+		const donelist = todo.donelist();
+		if (donelist.length !== 0) {
+			msg.send(donelist.join('\n'));
+		} else {
+			msg.send('(完了したTODOはありません)');
+		}
 	});
 };
