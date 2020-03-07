3b4f14085f59cb8ece16874d7d60ef2f3e4818e0
IBRGRL <iberigorilla@gmail.com>
2020-03-08 00:24:08 +0900

    改良

--- scripts/todo.js
+++ scripts/todo.js
@@ -25,17 +25,19 @@ module.exports = (robot) => {
 		msg.send('削除しました: ' + task);
 	});
 	robot.respond(/list/i, (msg) => {
-		if (todo.list().length === 0) {
+		const list = todo.list();
+		if (list.length === 0) {
 			msg.send('(TODOはありません)');
 		} else {
-			msg.send(todo.list().join('\n'));
+			msg.send(list.join('\n'));
 		}
 	});
 	robot.respond(/donelist/i, (msg) => {
-		if (todo.donelist().length === 0) {
+		const donelist = todo.donelist();
+		if (donelist.length === 0) {
 			msg.send('(完了したTODOはありません)');
 		} else {
-			msg.send(todo.donelist().join('\n'));
+			msg.send(donelist.join('\n'));
 		}
 	});
 };
