078db51d21fd49b21ba70224c279ebc0c9cb1b18
daiki8 <daiki1945815@gmail.com>
2020-04-04 15:34:49 +0900

    空リストに対応した

--- scripts/todo.js
+++ scripts/todo.js
@@ -25,9 +25,19 @@ module.exports = (robot) => {
 		msg.send('削除しました: ' + task);
 	});
 	robot.respond(/list/i, (msg) => {
-		msg.send(todo.list().join('\n'));
+		const l = todo.list();
+		if (l.length === 0) {
+			msg.send("TODOはありません");
+		} else {
+			msg.send(l.join('\n'));
+		}
 	});
 	robot.respond(/donelist/i, (msg) => {
-		msg.send(todo.donelist().join('\n'));
+		const l = todo.list();
+		if (l.length === 0) {
+			msg.send("完了したTODOはありません");
+		} else {
+			msg.send(l.join('\n'));
+		}
 	});
 };
