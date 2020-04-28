e44b8b2ae412548a427eeaad55e64e9fd84f97ea
nicoednnncovid19 <social.change.start.with.u@gmail.com>
2020-04-28 14:38:27 +0900

    リストが空の場合にメッセ－ジを表示する

--- scripts/todo.js
+++ scripts/todo.js
@@ -6,28 +6,34 @@
 //   ボット名 del      - TODO を消す
 //   ボット名 list     - TODO の一覧表示
 //   ボット名 donelist - 完了した TODO の一覧表示
-'use strict';
-const todo = require('todo');
-module.exports = (robot) => {
-	robot.respond(/todo (.+)/i, (msg) => {
-		const task = msg.match[1].trim();
-		todo.todo(task);
-		msg.send('追加しました: ' + task);
-	});
-	robot.respond(/done (.+)/i, (msg) => {
-		const task = msg.match[1].trim();
-		todo.done(task);
-		msg.send('完了にしました: ' + task);
-	});
-	robot.respond(/del (.+)/i, (msg) => {
-		const task = msg.match[1].trim();
-		todo.del(task);
-		msg.send('削除しました: ' + task);
-	});
-	robot.respond(/list/i, (msg) => {
-		msg.send(todo.list().join('\n'));
-	});
-	robot.respond(/donelist/i, (msg) => {
-		msg.send(todo.donelist().join('\n'));
-	});
+"use strict";
+const todo = require("todo");
+module.exports = robot => {
+  robot.respond(/todo (.+)/i, msg => {
+    const task = msg.match[1].trim();
+    todo.todo(task);
+    msg.send("追加しました: " + task);
+  });
+  robot.respond(/done (.+)/i, msg => {
+    const task = msg.match[1].trim();
+    todo.done(task);
+    msg.send("完了にしました: " + task);
+  });
+  robot.respond(/del (.+)/i, msg => {
+    const task = msg.match[1].trim();
+    todo.del(task);
+    msg.send("削除しました: " + task);
+  });
+  robot.respond(/list/i, msg => {
+    if (todo.list().length == 0) {
+      return "(TODOはありません)";
+    }
+    msg.send(todo.list().join("\n"));
+  });
+  robot.respond(/donelist/i, msg => {
+    if (todo.list().length == 0) {
+      return "(完了したTODOはありません)";
+    }
+    msg.send(todo.donelist().join("\n"));
+  });
 };
