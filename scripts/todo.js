9a38bae556a68e4c14cd60c4a914d6f46f2fa273
Keita Nakagawa <ke27.nakagawa@gmail.com>
2020-05-02 17:02:20 +0900

    TODOが空白のときの発言を追加2

--- scripts/todo.js
+++ scripts/todo.js
@@ -25,9 +25,19 @@ module.exports = (robot) => {
 		msg.send('削除しました: ' + task);
 	});
 	robot.respond(/list/i, (msg) => {
-		msg.send(todo.list().join('\n'));
+		const task = msg.match[1];
+		if (task === void 0) {
+			msg.send('TODOはありません');
+		} else {
+			msg.send(todo.list().join('\n'));
+
 	});
 	robot.respond(/donelist/i, (msg) => {
+		const task = msg.match[1];
+		if (task === void 0) {
+			msg.send('完了したTODOはありません');
+		} else {
 		msg.send(todo.donelist().join('\n'));
+		}
 	});
 };
