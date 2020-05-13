6f45fb41c321552238194b12abb99597a39648be
yu-nishimine <yu-nishimine@kddi.com>
2020-05-13 10:31:25 +0900

    complete

--- scripts/todo.js
+++ scripts/todo.js
@@ -25,9 +25,15 @@ module.exports = (robot) => {
 		msg.send('削除しました: ' + task);
 	});
 	robot.respond(/list/i, (msg) => {
+		if(todo.list().length === 0){
+			msg.send('(TODOはありません)');
+		}else
 		msg.send(todo.list().join('\n'));
 	});
 	robot.respond(/donelist/i, (msg) => {
+		if(todo.todolist().length === 0){
+			msg.send('(完了したTODOはありません)');
+		}else
 		msg.send(todo.donelist().join('\n'));
 	});
 };
