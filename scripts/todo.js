f19f4905fc0fe19a8ac3a800eaac02ea6fcbf023
hytotk <hytotk@gmail.com>
2020-05-13 19:35:08 +0900

    TODOリストが空でlist/donelistコマンドを実行した時にもボットが発言するように修正

--- scripts/todo.js
+++ scripts/todo.js
@@ -6,28 +6,34 @@
 //   ボット名 del      - TODO を消す
 //   ボット名 list     - TODO の一覧表示
 //   ボット名 donelist - 完了した TODO の一覧表示
-'use strict';
-const todo = require('todo');
+"use strict";
+const todo = require("todo");
 module.exports = (robot) => {
-	robot.respond(/todo (.+)/i, (msg) => {
-		const task = msg.match[1].trim();
-		todo.todo(task);
-		msg.send('追加しました: ' + task);
-	});
-	robot.respond(/done (.+)/i, (msg) => {
-		const task = msg.match[1].trim();
-		todo.done(task);
-		msg.send('完了にしました: ' + task);
-	});
-	robot.respond(/del (.+)/i, (msg) => {
-		const task = msg.match[1].trim();
-		todo.del(task);
-		msg.send('削除しました: ' + task);
-	});
-	robot.respond(/list/i, (msg) => {
-		msg.send(todo.list().join('\n'));
-	});
-	robot.respond(/donelist/i, (msg) => {
-		msg.send(todo.donelist().join('\n'));
-	});
+  robot.respond(/todo (.+)/i, (msg) => {
+    const task = msg.match[1].trim();
+    todo.todo(task);
+    msg.send("追加しました: " + task);
+  });
+  robot.respond(/done (.+)/i, (msg) => {
+    const task = msg.match[1].trim();
+    todo.done(task);
+    msg.send("完了にしました: " + task);
+  });
+  robot.respond(/del (.+)/i, (msg) => {
+    const task = msg.match[1].trim();
+    todo.del(task);
+    msg.send("削除しました: " + task);
+  });
+  robot.respond(/list/i, (msg) => {
+    const todos = todo.list();
+    msg.send(todos.length > 0 ? todos.join("\n") : "(TODO はありません)");
+  });
+  robot.respond(/donelist/i, (msg) => {
+    const doneTodos = todo.donelist();
+    msg.send(
+      doneTodos.length > 0
+        ? doneTodos.join("\n")
+        : "(完了した TODO はありません)"
+    );
+  });
 };
