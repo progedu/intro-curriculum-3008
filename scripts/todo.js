8b80247f9a20b41a387f0307bee244cf7c6fc750
hiroin <egamescape@gmail.com>
2020-05-17 20:54:57 +0900

    Fixed behavior of the list and donelist commands in hubot-todo.

--- scripts/todo.js
+++ scripts/todo.js
@@ -24,10 +24,18 @@ module.exports = (robot) => {
 		todo.del(task);
 		msg.send('削除しました: ' + task);
 	});
-	robot.respond(/list/i, (msg) => {
-		msg.send(todo.list().join('\n'));
-	});
-	robot.respond(/donelist/i, (msg) => {
-		msg.send(todo.donelist().join('\n'));
-	});
+  robot.respond(/list/i, msg => {
+    if(todo.list().length) {
+      msg.send(todo.list().join('\n'));
+    } else {
+      msg.send('(TODO はありません)');
+    }
+  });
+  robot.respond(/donelist/i, msg => {
+    if(todo.donelist().length) {
+      msg.send(todo.donelist().join('\n'));
+    } else {
+      msg.send('(完了した TODO はありません)');
+    }
+  });
 };
