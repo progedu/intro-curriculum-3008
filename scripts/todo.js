9dfd116ceb224c58e383dd6a7a06eec91e6d415d
yuyukichi <yuukichi777@outlook.jp>
2020-07-01 19:28:42 +0900

    異なるメッセージの処理を実装しました

--- scripts/todo.js
+++ scripts/todo.js
@@ -7,27 +7,37 @@
 //   ボット名 list     - TODO の一覧表示
 //   ボット名 donelist - 完了した TODO の一覧表示
 'use strict';
-const todo = require('todo');
-module.exports = (robot) => {
-	robot.respond(/todo (.+)/i, (msg) => {
-		const task = msg.match[1].trim();
-		todo.todo(task);
-		msg.send('追加しました: ' + task);
+const todo =require('todo');
+module.exports = robot => {
+  robot.hear(/todo (.+)/i, msg => {
+    const task = msg.match[1].trim();
+    todo.todo(task);
+    msg.send('追加しました:'+ task);
+  });
+  robot.hear(/done (.+)/i, msg => {
+    const task = msg.match[1].trim();
+    todo.done(task);
+    msg.send('完了にしました: ' + task);
+  });
+  robot.hear(/del (.+)/i, msg => {
+    const task = msg.match[1].trim();
+    todo.del(task);
+    msg.send('削除しました: ' + task);
+  });
+
+  robot.respond(/list/i, (msg) => {
+		if(msg === true){
+      msg.send(todo.list().join('\n'));
+      return
+		}
+			msg.send('TODOはありません！');
 	});
-	robot.respond(/done (.+)/i, (msg) => {
-		const task = msg.match[1].trim();
-		todo.done(task);
-		msg.send('完了にしました: ' + task);
-	});
-	robot.respond(/del (.+)/i, (msg) => {
-		const task = msg.match[1].trim();
-		todo.del(task);
-		msg.send('削除しました: ' + task);
-	});
-	robot.respond(/list/i, (msg) => {
-		msg.send(todo.list().join('\n'));
-	});
+	
 	robot.respond(/donelist/i, (msg) => {
-		msg.send(todo.donelist().join('\n'));
+		if(msg === true){
+      msg.send(todo.donelist().join('\n'));
+      return
+		}
+			msg.send('完了したTODOはありません！');
 	});
 };
