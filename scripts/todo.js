fa6ae9a269ae2de5bdface50a09a76097e6e6fb1
Fukuchi Kentaro <ken3ken@me.com>
2020-07-05 00:45:53 +0900

    完了・未完了リストが空の時に何も入ってないことを示すように変更

--- scripts/todo.js
+++ scripts/todo.js
@@ -25,9 +25,19 @@ module.exports = (robot) => {
 		msg.send('削除しました: ' + task);
 	});
 	robot.respond(/list/i, (msg) => {
-		msg.send(todo.list().join('\n'));
+		const str = todo.list().join('\n');
+		if (str.length === 0) {
+			msg.send('TODOはありません');
+		} else {
+			msg.send(str);
+		}
 	});
 	robot.respond(/donelist/i, (msg) => {
-		msg.send(todo.donelist().join('\n'));
+		const str = todo.donelist().join('\n');
+		if (str.length === 0) {
+			msg.send('完了したTODOはありません');
+		} else {
+			msg.send(str);
+		}
 	});
 };
