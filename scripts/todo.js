2a0dad364179df2d7b38393fabedc00f99d63512
Aozora Okada <bluesky125osmkk@gmail.com>
2020-10-03 15:40:35 +0900

    listとdonelistのコマンドを簡潔にした

--- scripts/todo.js
+++ scripts/todo.js
@@ -9,35 +9,42 @@
 'use strict';
 const todo = require('todo');
 module.exports = (robot) => {
+	// todoコマンドを実装
 	robot.respond(/todo (.+)/i, (msg) => {
 		const task = msg.match[1].trim();
 		todo.todo(task);
 		msg.send('追加しました: ' + task);
 	});
+
+	// doneコマンドを実装
 	robot.respond(/done (.+)/i, (msg) => {
 		const task = msg.match[1].trim();
 		todo.done(task);
 		msg.send('完了にしました: ' + task);
 	});
+
+	// delコマンドを実装
 	robot.respond(/del (.+)/i, (msg) => {
 		const task = msg.match[1].trim();
 		todo.del(task);
 		msg.send('削除しました: ' + task);
 	});
+
+	// listコマンドを実装
 	robot.respond(/list/i, (msg) => {
-		const list = todo.list();
-		if (list.length === 0) {
-			msg.send('(TODOはありません)');
-		} else {
+		if (todo.list().length) {
 			msg.send(todo.list().join('\n'));
+		} else {
+			msg.send('(TODOはありません)');
 		}
 	});
+
+	// donelistコマンドを実装
 	robot.respond(/donelist/i, (msg) => {
-		const donelist = todo.donelist();
-		if (donelist.length === 0) {
-			msg.send('(完了したTODOはありません)')
-		} else {
+		if (todo.donelist().length) {
 			msg.send(todo.donelist().join('\n'));
+		} else {
+			msg.send('(完了したTODOはありません)')
 		}
 	});
 };
