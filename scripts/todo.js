5e3b7d0b795609c8da483083f154130f3e3160e0
yoh <rapture6040@gmail.com>
2020-10-03 09:25:01 +0900

    練習問題

--- scripts/todo.js
+++ scripts/todo.js
@@ -24,10 +24,21 @@ module.exports = (robot) => {
 		todo.del(task);
 		msg.send('削除しました: ' + task);
 	});
-	robot.respond(/list/i, (msg) => {
-		msg.send(todo.list().join('\n'));
+
+	robot.respond(/list/i, msg => {
+		const list = todo.list();
+		if (list.length === 0 ) {
+			msg.send('(todoはありません)');
+		} else {
+			msg.send(todo.list().join('\n'));
+		}
 	});
+
 	robot.respond(/donelist/i, (msg) => {
+		if (todo.donelist().length) {
 		msg.send(todo.donelist().join('\n'));
+		} else {
+			msg.send("完了したタスクはありません")
+		}
 	});
 };
