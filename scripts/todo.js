588125e04306a14848d0e770b05331e0534401c9
maki-code <63586566+maki-code@users.noreply.github.com>
2020-11-06 17:32:26 +0900

    list,donelistが空の場合の処理を追加

--- scripts/todo.js
+++ scripts/todo.js
@@ -1,7 +1,7 @@
 // Description:
 //   TODO を管理することができるボットです
 // Commands:
-//   ボット名 todo     - TODO を作成
+//   ボット名 add      - TODO を作成
 //   ボット名 done     - TODO を完了にする
 //   ボット名 del      - TODO を消す
 //   ボット名 list     - TODO の一覧表示
@@ -9,9 +9,9 @@
 'use strict';
 const todo = require('todo');
 module.exports = (robot) => {
-	robot.respond(/todo (.+)/i, (msg) => {
+	robot.respond(/add (.+)/i, (msg) => {
 		const task = msg.match[1].trim();
-		todo.todo(task);
+		todo.add(task);
 		msg.send('追加しました: ' + task);
 	});
 	robot.respond(/done (.+)/i, (msg) => {
@@ -25,9 +25,17 @@ module.exports = (robot) => {
 		msg.send('削除しました: ' + task);
 	});
 	robot.respond(/list/i, (msg) => {
+		if(todo.list().length){
 		msg.send(todo.list().join('\n'));
+		} else {
+			msg.send("タスクはありません")
+		}
 	});
 	robot.respond(/donelist/i, (msg) => {
+		if(todo.list().length){
 		msg.send(todo.donelist().join('\n'));
+		} else {
+			msg.send("完了したタスクはありません")
+		}
 	});
 };
