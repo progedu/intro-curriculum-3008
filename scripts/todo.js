323d532528acccd2c66b91b82c9540ec638a9ab3
nakako <nakako.soft@gmail.com>
2021-03-11 05:48:46 +0900

    listとdonelistが空であるときの処理を追加

--- scripts/todo.js
+++ scripts/todo.js
@@ -25,9 +25,21 @@ module.exports = (robot) => {
 		msg.send('削除しました: ' + task);
 	});
 	robot.respond(/list/i, (msg) => {
-		msg.send(todo.list().join('\n'));
+		const tasks = todo.list();
+		if(tasks.length === 0){
+			msg.send('TODO はありません');
+		}
+		else{
+			msg.send(todo.list().join('\n'));
+		}
 	});
 	robot.respond(/donelist/i, (msg) => {
-		msg.send(todo.donelist().join('\n'));
+		const tasks = todo.donelist();
+		if(tasks.length === 0){
+			msg.send('完了した TODO はありません');
+		}
+		else{
+			msg.send(tasks.join('\n'));	
+		}
 	});
 };
