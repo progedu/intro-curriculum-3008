22f07cf30cb9a1835213e96128e895dfafd48acb
kyoko <kyokoakatsuka38@gmail.com>
2021-03-16 18:58:29 +0900

    todoなし、完了todoなし

--- scripts/todo.js
+++ scripts/todo.js
@@ -6,6 +6,8 @@
 //   ボット名 del      - TODO を消す
 //   ボット名 list     - TODO の一覧表示
 //   ボット名 donelist - 完了した TODO の一覧表示
+//	ボット名 nothingtodo - TODO はありません
+//	ボット名 nothingtocompletedtodo - 完了した TODO はありません
 'use strict';
 const todo = require('todo');
 module.exports = (robot) => {
@@ -25,9 +27,18 @@ module.exports = (robot) => {
 		msg.send('削除しました: ' + task);
 	});
 	robot.respond(/list/i, (msg) => {
-		msg.send(todo.list().join('\n'));
+		if(list.length === 0){
+			msg.send('(TODO はありません)');
+		}else{
+			msg.send(list.join('\n'));
+		}
 	});
 	robot.respond(/donelist/i, (msg) => {
-		msg.send(todo.donelist().join('\n'));
+		const donelist = todo.donelist();
+		if(donelist.length === 0){
+			msg.send('(完了した TODO はありません)');
+		}else{
+			msg.send(donelist.join('\n'));
+		}
 	});
 };
