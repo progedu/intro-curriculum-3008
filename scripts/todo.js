228202fd8332a398ea177a3ea011d7da08944180
syake5959 <aho.gnan2@gmail.com>
2021-04-12 22:23:27 +0900

    リストが空のときの反応を追加

--- scripts/todo.js
+++ scripts/todo.js
@@ -24,10 +24,20 @@ module.exports = (robot) => {
 		todo.del(task);
 		msg.send('削除しました: ' + task);
 	});
-	robot.respond(/list/i, (msg) => {
-		msg.send(todo.list().join('\n'));
+	robot.respond(/list/i, msg => {
+		const list = todo.list();
+		if (list.length === 0) {
+			msg.send('(TODOはありません)');
+		} else {
+			msg.send(list.join('\n'));
+		}
 	});
-	robot.respond(/donelist/i, (msg) => {
-		msg.send(todo.donelist().join('\n'));
+	robot.respond(/donelist/i, msg => {
+		const donelist = todo.donelist();
+		if (donelist.length === 0) {
+			msg.send('(完了したTODOはありません)');
+		} else {
+			msg.send(donelist.join('\n'));
+		}
 	});
 };
